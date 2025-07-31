88c80d5b801dd88f050b3869cc09cbbb8bc6486f
romanrios <romanrios@live.com>
2025-07-31 18:14:02 -0300

    wip

--- js/main.js
+++ js/main.js
@@ -1,5 +1,54 @@
+import { testimonios } from "./testimonios.js";
+
+// Parallax
 window.addEventListener('scroll', () => {
   const scrolled = window.scrollY;
   const image = document.querySelector('.parallax-img');
-  image.style.transform = `translateY(${scrolled * 0.3}px)`; // Ajustá el 0.3 según el efecto deseado
+  if (image) {
+    image.style.transform = `translateY(${scrolled * 0.3}px)`; // Ajustá el factor si querés más o menos efecto
+  }
+});
+
+
+
+
+
+// Carrusel Testimonios
+
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
+
+const container = document.querySelector('.swiper-wrapper');
+testimonios.forEach(t => {
+  container.innerHTML += `
+    <div class="swiper-slide">
+      <div class="testimonios_card">
+        <img src="${t.imagen}" alt="${t.nombre}" class="testimonios_img" />
+        <p class="testimonios_texto">"${t.mensaje}"</p>
+        <p class="testimonios_nombre">${t.nombre} - ${t.puesto}</p>
+        <p class="testimonios_institucion">${t.empresa}</p>
+      </div>
+    </div>
+  `;
 });
